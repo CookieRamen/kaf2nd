7b938871edea4f2bdfb9dd2dd8d644955aa714e4
CookieRamen <ark@arkjp.net>
2020-09-16 12:31:29 +0900

    Create history interface

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -5,6 +5,15 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
+
+interface History {
+  date: string;
+  title: string;
+  desc: string;
+  type: string;
+  url: string;
+}
+
 export class HistoryComponent implements OnInit {
 
   constructor() {
