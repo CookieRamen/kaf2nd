e5e6ad8512997266fe49e7158032432445c87cb6
CookieRamen <ark@arkjp.net>
2020-09-17 12:31:55 +0900

    Create history template.

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -15,6 +15,28 @@ interface History {
 }
 
 export class HistoryComponent implements OnInit {
+  /*
+    typeの種類
+    Twitter埋め込み: twitter
+    YouTube埋め込み: youtube
+    テキストのみ: text
+    その他: other
+  */
+  history: History[] = [
+    {
+      date: '2000/01/01',
+      title: 'サンプル',
+      desc: '"吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。<br>何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。<br>吾輩はここで始めて人間というものを見た。<br>しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。<br>この書生というのは時々我々を捕えて煮て食うという話である。"',
+      type: 'youtube',
+      url: 'https://www.youtube.com/watch?v=JxcKj2dMZM8'
+    }, {
+      date: '2000/01/01',
+      title: 'サンプル',
+      desc: '"吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。<br>何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。<br>吾輩はここで始めて人間というものを見た。<br>しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。<br>この書生というのは時々我々を捕えて煮て食うという話である。"',
+      type: 'youtube',
+      url: 'https://www.youtube.com/watch?v=JxcKj2dMZM8'
+    }
+  ];
 
   constructor() {
   }
